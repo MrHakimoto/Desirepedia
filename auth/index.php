37cef24c607ac74144cf92f00fd868216c88907f
MrHakimoto <fodp1116@gmail.com>
2022-05-24 18:04:56 -0300

    Configurações da aplicação

---
+++ auth/index.php
@@ -0,0 +1,3 @@
+<!--
+    Página de autênticação do usuário
+-->
---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- 
+    Inicio da aplicação, 
+    Página inicial onde o usuário terá acesso as provas dos vestibulares.
+-->
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Desirepédia | Home </title>
+</head>
+<body>
+    <?php echo "OLá mundi"; ?>
+</body>
+</html>
