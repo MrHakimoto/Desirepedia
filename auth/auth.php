9d4c47b5d57b488c0f109a28ad1f16ee45f4aa81
MrHakimoto <fodp1116@gmail.com>
2022-05-27 00:00:21 -0300

    Conclusão sistema auth do Google

--- auth/auth.php
+++ auth/auth.php
@@ -3,6 +3,8 @@ require_once '../vendor/autoload.php';
 
 use Google\Client as GoogleClient;
 
+$jwt = new \Firebase\JWT\JWT;
+$jwt::$leeway = 60;
 
 if(!isset($_POST['credential']) || !isset($_POST['g_csrf_token']) ){
     header("Location: index.php?error=1");
@@ -16,18 +18,32 @@ if($_POST['g_csrf_token'] != $cookie) {
 }
 
 //Cria instância
-$client = new GoogleClient(['client_id' => '31857203907-8isaavdi8cnt31ahmq8hcvgei1l700j1.apps.googleusercontent.com']); 
+$client = new GoogleClient(['client_id' => '931857203907-8isaavdi8cnt31ahmq8hcvgei1l700j1.apps.googleusercontent.com']); 
 $payload = $client->verifyIdToken($_POST['credential']);
+
 if (isset($payload['email'])) {
     echo "<pre>";
     print_r($payload);
     echo "</pre>";
+
+    session_start();
+    $_SESSION['nome'] =  $payload['given_name'];
+    $_SESSION['nome2'] = $payload['family_name'];
+    $_SESSION['email'] = $payload['email'];
+    $_SESSION['photo'] = $payload['picture'];
+
+    echo $_SESSION['nome'];
+
+    echo "<br>";
+    echo $_SESSION['email']; 
+    echo "<br>";
+    echo $_SESSION['photo'];
+    
+
+
+
 }
 
-
-print_r($cookie);
-
-print_r($_POST);
-
-
 ?>
+
+<img src="<?php echo $_SESSION['photo']; ?>" alt="">
